33d3b46ccbe2b3582c161975a88c1fb698d7c760
Benny <empty>
2023-02-22 22:32:55 +0100

    fix everything

---
+++ src/main/java/guiView.java
@@ -0,0 +1,219 @@
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+
+import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.dnd.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+
+public class guiView extends JTable implements DropTargetListener {
+
+    public JFrame gui;
+    private JMenuItem addFile, delFile, showCloud, authorize, cred, howTo, disc, exit;
+    private JMenuBar menuBar;
+    private JMenu startMenu, helpMenu;
+    JLabel label, dropLabel;
+    private JTabbedPane tabpane;
+    private JTree tree;
+    private DefaultMutableTreeNode rootNode;
+    JPanel dropPanel;
+    Object[][] data = {};
+    DefaultTableModel tableModel;
+    JTable table;
+    JScrollPane scrollPane;
+
+    public guiView() {
+        gui = new JFrame();
+        gui.setBounds(100, 100, 800, 600);
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setLocationRelativeTo(null);
+        gui.setResizable(false);
+        JMenuBar menuBar = new JMenuBar();
+        startMenu = new JMenu("Start");
+        addFile = new JMenuItem("Datei hochladen");
+        delFile = new JMenuItem("Datei loeschen");
+        showCloud = new JMenuItem("Cloud anzeigen");
+        authorize = new JMenuItem("Mit der Cloud verbinden");
+        disc = new JMenuItem("Verbindung trennen");
+        exit = new JMenuItem("Exit");
+        helpMenu = new JMenu("Hilfe");
+        cred = new JMenuItem("Credits");
+        howTo = new JMenuItem("Bedienung");
+        startMenu.add(addFile).setEnabled(false);
+        startMenu.add(delFile).setEnabled(false);
+        startMenu.add(showCloud).setEnabled(false);
+        startMenu.add(disc).setEnabled(false);
+        startMenu.add(authorize);
+        startMenu.add(disc);
+        startMenu.add(exit);
+        helpMenu.add(cred);
+        helpMenu.add(howTo);
+        menuBar.add(startMenu);
+        menuBar.add(helpMenu);
+        gui.setJMenuBar(menuBar);
+        gui.setVisible(true);
+        dropPanel = new JPanel(new BorderLayout());
+        dropLabel = new JLabel("Drop files here");
+        dropLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        dropLabel.setFont(new Font("Arial", Font.BOLD, 20));
+
+        authorize.addActionListener(e -> {
+            gdriveAuthorize();
+            addFile.setEnabled(true);
+            delFile.setEnabled(true);
+            showCloud.setEnabled(true);
+            authorize.setEnabled(false);
+            disc.setEnabled(true);
+
+            JPanel addFileTab = new JPanel(new BorderLayout());
+            addFileTab.setPreferredSize(new Dimension(500, 300));
+            tabpane = new JTabbedPane(JTabbedPane.TOP,JTabbedPane.SCROLL_TAB_LAYOUT );
+            gui.add(tabpane);
+            scrollPane = new JScrollPane(table);
+            scrollPane.setPreferredSize(new Dimension(800, 300));
+            addFileTab.add(scrollPane, BorderLayout.CENTER);
+            table.setModel(tableModel);
+        });
+
+        showCloud.addActionListener(e -> listTreeFiles());
+        addFile.addActionListener(e -> {
+            dropPanel.setDropTarget(new DropTarget(dropPanel, DnDConstants.ACTION_COPY, this));
+            dropPanel.setPreferredSize(new Dimension(500, 300));
+            dropPanel.add(dropLabel, BorderLayout.NORTH);
+            gui.add(dropPanel);
+            tabpane.addTab("Datei hinzufuegen", dropPanel);
+            tabpane.setSelectedIndex(tabpane.getTabCount()-1);
+        });
+
+        exit.addActionListener(e -> System.exit(0));
+    }
+
+    public void gdriveAuthorize() {
+        try {
+            gdrive.getDriveService();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (GeneralSecurityException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    public void listTreeFiles() {
+        try {
+            Drive service = gdrive.getDriveService();
+            FileList result = service.files().list()
+                    .setFields("nextPageToken, files(name, size, mimeType, createdTime, id, parents)")
+                    .execute();
+            List<File> files = result.getFiles();
+
+            // Erstelle den Wurzelknoten, wenn er noch nicht existiert
+            if (rootNode == null) {
+                rootNode = new DefaultMutableTreeNode("Ordner");
+                tree = new JTree(rootNode);
+                JScrollPane scrollPane = new JScrollPane(tree);
+                scrollPane.setPreferredSize(new Dimension(800, 300));
+                tabpane.add("Cloud Overview", scrollPane);
+                tabpane.setSelectedIndex(tabpane.getTabCount() - 1);
+            }
+
+            // Aktualisiere den Wurzelknoten mit den neuen Dateien
+            rootNode.removeAllChildren();
+            for (File file : files) {
+                DefaultMutableTreeNode fileNode = new DefaultMutableTreeNode(file.getName());
+                rootNode.add(fileNode);
+            }
+            ((DefaultTreeModel) tree.getModel()).reload();
+
+        } catch (IOException | GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void listFiles() {
+        try {
+            Drive service = gdrive.getDriveService();
+            FileList result = service.files().list()
+                    .setFields("nextPageToken, files(name, size, mimeType, createdTime)")
+                    .execute();
+            List<File> files = result.getFiles();
+
+            int columnNames = 0;
+            DefaultTableModel newModel = new DefaultTableModel(columnNames, 0);
+            for (int i = 0; i < files.size(); i++) {
+                File file = files.get(i);
+                Object[] rowData = {file.getName(), file.getSize(), file.getMimeType(), file.getCreatedTime()};
+                newModel.addRow(rowData);
+            }
+            table.setModel(newModel);
+
+        } catch (IOException | GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void dragEnter(DropTargetDragEvent e) {
+        if (e.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+            e.acceptDrag(DnDConstants.ACTION_COPY);
+            dropLabel.setText("dragEnter");
+        } else {
+            e.rejectDrag();
+        }
+    }
+
+    @Override
+    public void dragExit(DropTargetEvent dte) {
+        dropLabel.setText("dragExit");
+    }
+
+    @Override
+    public void dragOver(DropTargetDragEvent e) {
+        if (e.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+            e.acceptDrag(DnDConstants.ACTION_COPY);
+            dropLabel.setText("DdragOver");
+        } else {
+            e.rejectDrag();
+        }
+    }
+
+    @Override
+    public void dropActionChanged(DropTargetDragEvent dropTargetDragEvent) {
+    }
+
+    @Override
+    public void drop(DropTargetDropEvent e) {
+        e.acceptDrop(DnDConstants.ACTION_COPY);
+        try {
+            Transferable t = e.getTransferable();
+            java.util.List<java.io.File> files = (java.util.List<java.io.File>) t.getTransferData(DataFlavor.javaFileListFlavor);
+            for (java.io.File file : files) {
+                // Hier können Sie den Code einfügen, um die Datei hochzuladen oder zu speichern
+                String hallo = file.getAbsolutePath();
+                System.out.println(file.getName() + hallo);
+            }
+            dropLabel.setText("drop");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        e.dropComplete(true);
+    }
+    public static void main(String[] args) {
+        guiView gui = new guiView();
+        gui.setVisible(true);
+    }
+}
