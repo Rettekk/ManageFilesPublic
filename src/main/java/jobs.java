45a6e50667264fa511a0e26877abbbfb6b00f341
Benny <empty>
2023-03-02 11:22:47 +0100

    jobs java

---
+++ src/main/java/jobs.java
@@ -0,0 +1,22 @@
+public class jobs {
+    String[] ausbildungsberufe = {"Anlagenmechaniker", "Automobilkaufmann", "Bäcker", "Bankkaufmann", "Bauzeichner",
+            "Berufskraftfahrer", "Chemielaborant", "Dachdecker", "Elektroniker für Betriebstechnik",
+            "Ergotherapeut", "Fachinformatiker", "Fachkraft für Lagerlogistik", "Fahrzeuglackierer",
+            "Feinwerkmechaniker", "Fliesenleger", "Friseur", "Gebäudereiniger", "Gärtner",
+            "Gesundheits- und Krankenpfleger", "Groß- und Außenhandelskaufmann", "Hauswirtschafter",
+            "Hotelfachmann", "Immobilienkaufmann", "Industriekaufmann", "Industriemechaniker",
+            "IT-Systemelektroniker", "Justizfachangestellter", "Kauffrau/-mann für Büromanagement",
+            "Kauffrau/-mann im Einzelhandel", "Kauffrau/-mann im Groß- und Außenhandel",
+            "Kfz-Mechatroniker", "Klempner", "Konstruktionsmechaniker", "Kosmetiker", "Kraftfahrzeugmechatroniker",
+            "Lacklaborant", "Land- und Baumaschinenmechatroniker", "Maler und Lackierer", "Mechatroniker",
+            "Mediengestalter Digital und Print", "Medizinische Fachangestellte", "Metallbauer",
+            "Maurer", "Notarfachangestellter", "Orthopädieschuhmacher", "Packmitteltechnologe",
+            "Pharmakant", "Pflanzentechnologe", "Raumausstatter", "Rechtsanwaltsfachangestellter",
+            "Restaurantfachmann", "Schneider", "Speditionskaufmann", "Steinmetz und Steinbildhauer",
+            "Straßenbauer", "Stuckateur", "Systemgastronom", "Technischer Produktdesigner",
+            "Technischer Systemplaner", "Tiermedizinische Fachangestellte", "Tischler", "Veranstaltungskaufmann",
+            "Verfahrensmechaniker", "Verkäuferin", "Versicherungskaufmann", "Vermessungstechniker",
+            "Verwaltungsfachangestellter", "Werkzeugmechaniker", "Winzer", "Wirtschaftsinformatiker",
+            "Wirtschaftsprüfer", "Zahnmedizinische Fachangestellte", "Zerspanungsmechaniker",
+            "Zimmerer", "Zweiradmechaniker"};
+}
